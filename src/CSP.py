be16afe0dba44fa9ce8a4fbf05763b9a1518ed55
Yue0925 <mellet1997@outlook.com>
2021-12-01 19:53:54 +0100

    model CSP

--- src/CSP.py
+++ src/CSP.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+
+VARIABLES_SELECTION = ["arbitrary"] # d'autres possibilités à compléter
+VALUES_SELECTION = ["arbitrary"] # d'autres possibilités à compléter
+
 """ Implementation of a variable object. For the sake of simplicity, all attributes are public.
 """
 class Variable(object):
@@ -12,17 +16,19 @@ class Variable(object):
         self.domMin = 0
         self.domMax = 0
         self.domEnum = set() # domaine énumérée, l'ens élément finis
-        #self.domFun = function # domaine définie par une fonction
+        #def domFun():
+        #    pass
+        #self.domFun = domFun # domaine définie par une fonction
 
-    def __init__(self, id, name, domMin, domMax, domEnum):
+    def __init__(self, id : int, name : str, domMin : int, domMax : int): #, domFun):
         """ Initialize a variable."""
         self.id = id
         self.name = name #facultatif
 
         self.domMin = domMin
         self.domMax = domMax
-        self.domEnum = domEnum # domaine énumérée, l'ens élément finis
-        #self.domFun = function # domaine définie par une fonction
+        self.domEnum = set(range(domMin, domMax+1)) # domaine énumérée, l'ens élément finis
+        #self.domFun = domFun # domaine définie par une fonction
 
 
     
@@ -36,12 +42,17 @@ class Constraint(object):
 
         self.feasibleTuples = set() # l'ensemble couples admissibles
 
-    def __init__(self, var1, var2, feasibleTuples):
+    def __init__(self, var1:Variable, var2 : Variable):
         """ Initialize a constraint."""
         self.var1 = var1
         self.var2 = var2
 
-        self.feasibleTuples = feasibleTuples # l'ensemble couples admissibles
+        self.feasibleTuples = set() # l'ensemble couples admissibles
+        for a in var1.domEnum: 
+            for b in var2.domEnum : self.feasibleTuples.add((a, b))
+
+    def __repr__(self):
+        return "constraint ({0}, {1})".format(self.var1.name, self.var2.name)
 
 
 """ Implementation of an integer Constraint Satisfaction Programming Solver. 
@@ -53,15 +64,26 @@ class CSP(object):
         self.nbVars = 0
         self.vars = list() # list of variables
 
-        self.nbConsts = 0
-        self.consts = list() # list of constraints
+        self.nbConstrs = 0
+        self.constrs = list() # list of constraints
 
-    def __init__(self, nbVars, vars, nbConsts, consts):
+    def __init__(self, nbVars : int, vars : list, nbConstrs : int, constrs : list):
+        """ Initialize a CSP."""
         self.nbVars = nbVars
         self.vars = vars # list of variables
 
-        self.nbConsts = nbConsts
-        self.consts = consts # list of constraints
+        self.nbConstrs = nbConstrs
+        self.constrs = constrs # list of constraints
+
+    def add_variable(self, var:Variable):
+        """ Add a variable to CSP. """
+        self.nbVars += 1
+        self.vars.append(var)
+
+    def add_constraint(self, constr:Constraint):
+        """ Add a constraint to CSP. """
+        self.nbConstrs += 1
+        self.constrs.append(constr)
 
 
 
--- src/test.py
+++ src/test.py
@@ -4,22 +4,30 @@
 
 from CSP import *
 
+def domFun():
+    pass
+
+
+
 if __name__ == "__main__":
-    print(" here ! ")
+    print(" test.py ! ")
     var1 = Variable(
         0,
         "var1",
         0,
-        1,
-        set()
+        1
     )
     var2 = Variable(
         1,
         "var2",
         0,
-        1,
-        set()
+        1
     )
 
-    const1 = Constraint(var1, var2, set())
+    const1 = Constraint(var1, var2)
     myCSP = CSP(2, [var1, var2], 1, const1)
+
+    print(const1)
+    for (a, b) in const1.feasibleTuples:
+        print("(", a, ", ", b, ")")
+    
